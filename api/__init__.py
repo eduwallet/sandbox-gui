f647a75bc4cb8045c6c121aea1dedb7a28efd8a5
Sander Hollaar <sander@humilis.net>
2025-11-12 08:31:25 +0100

    - open config.json in $CONF_PATH to fix: No such file or directory: config.json

--- api/__init__.py
+++ api/__init__.py
@@ -19,5 +19,5 @@ for file in os.listdir('tests'):
     name = file[:-5]
     testset[name] = tests
 
-with open('config.json') as data:
+with open(os.getenv('CONF_PATH', '') + 'config.json') as data:
   config = json.load(data)
