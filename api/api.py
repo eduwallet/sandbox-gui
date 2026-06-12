e91b81d7e6386c46300a8db9f73e697e87fe4473
Martin van Es <martin@mrvanes.com>
2026-06-12 10:38:23 +0200

    Fix SWITCH

--- api/api.py
+++ api/api.py
@@ -415,3 +415,141 @@ def eduid_status():
           res = json.loads(f.read().decode())
 
   return json.dumps(status)
+
+
+@api.route("/switch")
+def switch():
+  form = session.get('form')
+  if form is None:
+      logging.error("switch request failed")
+      message = {
+          "status": "error"
+      }
+      return json.dumps(message)
+
+  test_file = form.get('test_file')
+  test_id = form.get('test_id')
+  test = testset[test_file][test_id]
+
+  credential_type = test['type']
+  credential_input = test['input']
+
+  data = {
+      'credentials': [credential_type],
+      'grants': {
+          'authorization_code': {
+              'issuer_state': 'generate',
+          },
+      },
+      'credentialDataSupplierInput': credential_input,
+  }
+
+  tx_code_len = form.get('tx_code_len')
+  if int(tx_code_len) > 0:
+    tx_code_mode = form.get('tx_code_mode')
+    tx_code = {
+        'length': int(tx_code_len),
+        'input_mode': tx_code_mode
+    }
+  else:
+    tx_code = False
+
+  data['grants']['authorization_code']['tx_code'] = tx_code
+
+  json_data = json.dumps(data).encode("utf-8")
+
+  create_url = config['switch']['url'] + "/create-offer"
+
+  headers = {
+      "Content-Type": "application/json",
+      "Authorization": f"Bearer {config['switch']['token']}"
+  }
+
+  # print(f'create_url: {create_url}')
+  # print(headers)
+  # print(json_data)
+
+  req = urllib.request.Request(create_url, json_data, headers)
+  with urllib.request.urlopen(req, context=context) as f:
+    res = json.loads(f.read().decode())
+
+  qr_uri = res['uri']
+  pin = res.get('txCode')
+  state = res.get('id')
+
+  message = {
+      "status": "success",
+      "test": test,
+      "qr_uri": qr_uri,
+      "pin": pin,
+      "data": data
+  }
+
+  session['revoke'] = True if form.get('revoke') else False
+  session['state'] = state
+
+  return json.dumps(message)
+
+
+@api.route("/switch_status")
+def switch_status():
+  state = session.get('state')
+  if state is None:
+    logging.error("pac_status request failed")
+    message = {
+        "status": "error"
+    }
+    return json.dumps(message)
+
+  revoke = session.get('revoke')
+
+  # print(revoke)
+
+  data = {
+      'id': state
+  }
+
+  json_data = json.dumps(data).encode("utf-8")
+
+  check_url = config['switch']['url'] + "/check-offer"
+
+  headers = {
+      "Content-Type": "application/json",
+      "Authorization": f"Bearer {config['switch']['token']}"
+  }
+
+  # print(f'check_url: {check_url}')
+  # print(headers)
+  # print(json_data)
+
+  req = urllib.request.Request(check_url, json_data, headers)
+
+  with urllib.request.urlopen(req, context=context) as f:
+      res = json.loads(f.read().decode())
+
+  # print(res)
+
+  status = res['status']
+
+  if status == 'CREDENTIAL_ISSUED' and revoke:
+      uuid = res['uuid']
+      data = {
+          "uuid": uuid,
+          "state": 'revoke'
+          # list: <optional URI of a specific statuslist for which to set/unset the status>
+      }
+
+      headers = {
+          "Content-Type": "application/json",
+          "Authorization": f"Bearer {config['switch']['token']}"
+      }
+
+      json_data = json.dumps(data).encode("utf-8")
+
+      revoke_url = config['switch']['url'] + "/revoke-credential"
+
+      req = urllib.request.Request(revoke_url, json_data, headers)
+      with urllib.request.urlopen(req, context=context) as f:
+          res = json.loads(f.read().decode())
+
+  return json.dumps(status)
