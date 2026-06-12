e6605e0ed99bd9dfcbb1b7b64b52d5e8f162905a
Martin van Es <martin@mrvanes.com>
2026-06-12 10:16:56 +0200

    Add SWITCH eduID test

--- api/__init__.py
+++ api/__init__.py
@@ -7,12 +7,14 @@ testset = {}
 for file in sorted(os.listdir('tests')):
   if file.endswith(".json"):
     with open('tests/' + file) as data:
+      print(f"loading {file}")
       tests = json.load(data)
 
     for k, v in tests.items():
       credential = v.get('credential')
       if credential:
         with open(f'tests/credentials/{credential}') as data:
+          print(f" - loading {credential}")
           cred = json.load(data)
           v['credential'] = cred
 
@@ -20,4 +22,5 @@ for file in sorted(os.listdir('tests')):
     testset[name] = tests
 
 with open(os.getenv('CONF_PATH', '') + 'config.json') as data:
+  print('loading config.json')
   config = json.load(data)
